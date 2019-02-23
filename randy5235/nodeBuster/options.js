4bbb061af6f58b6a5f6459b79c7bb54758988f03
Randy Wressell <randy5235@gmail.com>
2019-02-23 01:09:37 -0800

    Update linting

--- randy5235/nodeBuster/options.js
+++ randy5235/nodeBuster/options.js
@@ -2,28 +2,28 @@
 const package = require('./package.json');
 
 // purposeful destructuring to remove first two elements
-const [, , ...flags] = process.argv; 
+const [, , ...flags] = process.argv;
 
 module.exports = (() => {
   const flagObject = flags.reduce((prev, current, index) => {
     if (/^-h/.test(current) || /^--help/.test(current)) {
-      return {...prev, help:true};
+      return {...prev, help: true};
     }
     if (/^-v/.test(current) || /^--version/.test(current)) {
-      return {...prev, version:true};
+      return {...prev, version: true};
     }
     if (/^-{1,2}(.+)/.test(current)) {
-      return {...prev, [/^-{1,2}(.*)/.exec(current)[1]]: flags[index+1]};
+      return {...prev, [/^-{1,2}(.*)/.exec(current)[1]]: flags[index + 1]};
     }
     if (/^https?\:\/\//.test(current)) {
       return {...prev, baseUrl: current};
     }
     return {...prev};
-  }, {})
+  }, {});
 
   if (flagObject.version) {
     console.log(package.version);
-        process.exit(0);
+    process.exit(0);
   }
 
   if (flagObject.help) {
