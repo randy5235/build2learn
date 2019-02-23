3bada3a40f95c77b06a5246f93b68d668c4035ab
Kaelyn <flowerdaykaelyn@gmail.com>
2019-02-22 19:09:46 -0800

    added CLI argument catching and --help documentation.

--- kaelynflow/pybuster/pybuster.py
+++ kaelynflow/pybuster/pybuster.py
@@ -1,6 +1,28 @@
 """PyBuster
+
+Usage:
+   pybuster.py [-d <file>] <url>
+   pybuster.py (-h | --help)
+   pybuster.py --version
+
+Options:
+   -d <file>   Use directory names from file.
+   -h --help   Show this screen.
+   --version   Show version.
 """
+from docopt import docopt
+
+
 __version__ = "0.0.1"
 
-if __name__ == "__main__":
+
+def handler(url, dir_file=None):
     pass
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version="PyBuster 0.0.1")
+    url = arguments["URL"]
+    dir_file = arguments["-d"]
+    
+    handler(url, dir_file=dir_file)
