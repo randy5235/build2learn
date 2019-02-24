5914decacafd105e9d51c891c5716514757b3a6b
Kaelyn <flowerdaykaelyn@gmail.com>
2019-02-24 10:40:23 -0800

    add docstring to manager function.

--- kaelynflow/pybuster/pybuster.py
+++ kaelynflow/pybuster/pybuster.py
@@ -16,11 +16,26 @@ from docopt import docopt
 __version__ = "0.0.1"
 
 
-def handler(url, dir_file=None):
+def manager(url, dir_file=None):
+    """Takes server URL, scans for directories, and writes responses to file.
+    
+    If dir_file is specified makes requests for directories listed in dir_file, 
+    otherwise performs a brute force scan.
+    
+    Args:
+        url: The URL of the server to scan.
+        dir_file: Path for text file containing one directory name per line.
+    
+    Returns:
+        None
+    """
     pass
 
 
 if __name__ == "__main__":
     arguments = docopt(__doc__, version="PyBuster 0.0.1")
 
-    handler(arguments["URL"], dir_file=arguments["-d"])
+    if arguments["-d"] is not None:
+        manager(arguments["<url>"], dir_file=arguments["<file>"])
+    else:
+        manager(arguments["<url>"])
