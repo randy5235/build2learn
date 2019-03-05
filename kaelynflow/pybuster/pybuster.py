0b103c207b1623e1bcffef5b2f1fc005e1eb924a
Kaelyn <flowerdaykaelyn@gmail.com>
2019-03-04 20:33:38 -0800

    implement output file name cli option

--- kaelynflow/pybuster/pybuster.py
+++ kaelynflow/pybuster/pybuster.py
@@ -1,14 +1,15 @@
 """PyBuster
 
 Usage:
-   pybuster.py [-d <file>] <url>
+   pybuster.py [-w <wordlist>] [-o <out>] <url>
    pybuster.py (-h | --help)
    pybuster.py --version
 
 Options:
-   -d <file>   Use directory names from file.
-   -h --help   Show this screen.
-   --version   Show version.
+   -w <wordlist>   Name of file containing directory names to use.
+   -o <out>     File name to write output to.
+   -h --help       Show this screen.
+   --version       Show version.
 """
 from external.docopt import docopt
 
@@ -16,16 +17,17 @@ from external.docopt import docopt
 __version__ = "0.0.1"
 
 
-def manager(url, dir_file=None):
+def manager(url, wordlist=None, outfile=None):
     """Takes server URL, scans for directories, and writes responses to file.
-    
-    If dir_file is specified makes requests for directories listed in dir_file, 
+
+    If dir_file is specified makes requests for directories listed in dir_file,
     otherwise performs a brute force scan.
-    
+
     Args:
         url: The URL of the server to scan.
-        dir_file: Path for text file containing one directory name per line.
-    
+        wordlist: Path for text file containing one directory name per line.
+        outfile: File name to write output to.
+
     Returns:
         None
     """
@@ -33,9 +35,17 @@ def manager(url, dir_file=None):
 
 
 if __name__ == "__main__":
-    arguments = docopt(__doc__, version="PyBuster 0.0.1")
+    arguments = docopt(__doc__, version="PyBuster "+__version__)
 
-    if arguments["-d"] is not None:
-        manager(arguments["<url>"], dir_file=arguments["<file>"])
+    #check input for option flags and get associated arguments
+    if arguments["-w"] is not None:
+        wfile = arguments["<wordlist>"]
     else:
-        manager(arguments["<url>"])
+        wfile = None
+
+    if arguments["-o"] is not None:
+        out = arguments["<out>"]
+    else:
+        out = None
+
+    manager(arguments["<url>"], wordlist=wfile, outfile=out)
