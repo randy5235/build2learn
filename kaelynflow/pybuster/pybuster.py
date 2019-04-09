81c4c69f82ebe228cd6fdc16c266a033727944b9
Kaelyn <flowerdaykaelyn@gmail.com>
2019-04-08 18:28:44 -0700

    add minimum viable product for directory scanning functionality

--- kaelynflow/pybuster/pybuster.py
+++ kaelynflow/pybuster/pybuster.py
@@ -11,6 +11,9 @@ Options:
    -h --help       Show this screen.
    --version       Show version.
 """
+import os.path
+import time
+import requests
 from external.docopt import docopt
 
 
@@ -33,22 +36,46 @@ def main(url, wordlist=None, out=None):
     Returns:
         None
     """
-    pass
+    #TODO(kaelynflow): Thorough checking/sanitization
+    if url[-1] != "/":
+        url += "/"
+
+    if wordlist is None:
+        wordlist = "resources/wordlist_medium.txt"
+
+    if out is None:
+        out = "pybuster_out.txt"
+
+    #TODO(kaelynflow): Implement quiet error handling and logging
+    if not os.path.exists(wordlist):
+        err_msg = "No such file or directory: '{0}'".format(wordlist)
+        raise FileNotFoundError(err_msg)
+
+    response_dict = {} # holds url/response code pairs
+
+    with open(wordlist, "r") as infile:
+        #TODO(kaelynflow): Implement quiet error handling and logging
+        for dir_name in infile:
+            dir_url = url + dir_name.strip() + "/"
+            response = requests.get(dir_url)
+
+            start_time = time.time()
+            # If rate-limited, try waiting 10ms intervals
+            while response.status_code == 429 and time.time() - start_time < 1:
+                time.sleep(0.01)
+                response = requests.get(dir_url)
+
+            response_dict[response.url] = response.status_code
+
+
+    with open(out, "w") as outfile:
+        for dir_url in response_dict:
+            line = dir_url + " " + str(response_dict[dir_url]) + "\n"
+            outfile.write(line)
 
 
 if __name__ == "__main__":
     PKG_VER = "PyBuster " + __version__
-    ARGS = docopt(__doc__, version=PKG_VER)
+    args = docopt(__doc__, version=PKG_VER)
 
-    #check input for option flags and get corresponding arguments
-    if ARGS["-w"] is not None:
-        WORDLIST = ARGS["<wordlist>"]
-    else:
-        WORDLIST = None
-
-    if ARGS["-o"] is not None:
-        OUT = ARGS["<out>"]
-    else:
-        OUT = None
-
-    main(ARGS["<url>"], wordlist=WORDLIST, out=OUT)
+    main(args["<url>"], wordlist=args["-w"], out=args["-o"])
